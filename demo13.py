294dfe075056114ce136e981eda885b385bd6d86
wnmwi <383929371@qq.com>
2020-09-22 16:10:25 +0800

    test

---
+++ demo13.py
@@ -0,0 +1,19 @@
+print 'abc', 'xyz'
+x,y=1,2
+print "Value of x,y: ",x,y
+
+#字段
+dict = {'a':1,'b':2,'b':'3'}
+print dict['b']
+print dict
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+del dict['Name']
+dict.clear()
+del dict
+
+#print "dict['Age']: ", dict['Age']
+#print "dict[School]: ", dict['School']
+
+dict ={"Name":"Kitty","Age":"21","Name":"haha"}
+print dict["Name"]
